43de15cb4963ac359f8760c19bc8d0612ad720d6
kritten <stuff@kritten.org>
2019-09-14 10:30:34 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      {{isoDate}}
+      <v-date-picker v-bind:value="isoDate"></v-date-picker>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    // isoDate: '2019-06-06',
+    isoDate: new Date().toISOString(),
+  }),
+};
+</script>
